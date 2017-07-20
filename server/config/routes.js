a989868d471b5035debe536f69c0b3720597388d
Jason <rhuidean06@gmail.com>
2017-07-20 17:45:44 -0500

    add user model routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,1 +1,15 @@
+let path = require('path');
+let Users = require('../controllers/users');
+
+module.exports = function(app){
+	app.get('/users', Users.index);
+	app.post('/users', Users.create);
+    app.get('/users/:id',Users.show);
+    app.delete('/users/:id',Users.logout);
+    app.delete('/users/:id',Users.destroy);
+    app.get('/session',Users.session);
+    app.post('/session',Users.authenticate);
+    app.delete('/session',Users.logout);
+}
+
 
