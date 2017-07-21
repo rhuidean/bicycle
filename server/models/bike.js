7c5265bf965bcee4d0357f82573de3742870a63a
Jason <rhuidean06@gmail.com>
2017-07-21 13:17:26 -0500

    add bike model

---
+++ server/models/bike.js
@@ -0,0 +1,35 @@
+let mongoose = require('mongoose');
+
+let BikeSchema = new mongoose.Schema({
+	title: {
+		type: String,
+        required: [true, 'Title cannot be blank.']
+	},
+	description: {
+        type: String,
+        required: [true, 'Description cannot be blank.'],
+        maxlength: [200,'Description cannot exceed 200 characters.']        
+    },
+
+	location: {
+        type: String,
+		required: [true, "Password cannot be blank."]
+    },
+
+    price: [{
+        type: Number,
+        ref: [1,'Price must be at least $1.00']
+    }]
+    
+},{timestamps: true});
+
+UserSchema.pre('save', function(next){
+	this.password =bcrypt.hashSync(this.password, bcrypt.genSaltSync(10));
+	next();
+});
+
+UserSchema.methods.authenticate = function(password){
+	return bcrypt.compareSync(password, this.password);
+}
+
+mongoose.model('User', UserSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -10,13 +10,13 @@ let UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "Email cannot be blank."],
         unique: [true,"Email is already registered"],
-        // validate: {
-        //     validator: function(email){
-        //         let re=/^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-        //         return (email==null||email.trim().length <1)||re.test(email)
-        //     },
-        //     message: 'Provided mail is invalid.'
-        // }        
+        validate: {
+            validator: function(email){
+                let re=/^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                return (email==null||email.trim().length <1)||re.test(email)
+            },
+            message: 'Provided mail is invalid.'
+        }        
     },
 
 	password: {
@@ -24,7 +24,12 @@ let UserSchema = new mongoose.Schema({
         minlength: [4,"password cannot be less than 4 characters"],
         maxlength: [12,"password cannot be mor than 12 characters"],
 		required: [true, "Password cannot be blank."]
-    }
+    },
+
+    bikes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Bike'
+    }]
     
 },{timestamps: true});
 
